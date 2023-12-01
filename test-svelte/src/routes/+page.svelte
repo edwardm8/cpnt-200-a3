d6805fd01ef9befe726037cb66ac01975468f3e6
edwardm8 <edwardm825@gmail.com>
2023-11-30 18:46:11 -0700

    inital commit

---
+++ test-svelte/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let data;
+
+  let { blog_post } = data
+
+</script>
+
+{#each blog_post as blog_post}
+  <h1>{blog_post.title}</h1>
+  <h2>By:{blog_post.author}</h2>
+  <h2>Posted:{blog_post.date_created}</h2>
+  <h3>Tags:{blog_post.tags}</h3>
+  <p>{blog_post.text_box}</p>
+  <h3>Summary</h3>
+  <p>{blog_post.summary}</p>
+{/each}
